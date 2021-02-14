2286b6dd0523631a716f44326d3d0ab5d2da2363
mmmes <mmmesiarovamaja@gmail.com>
2021-02-14 18:06:16 +0100

    about, contact

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.layout')
 
 @section('content')
-    <h4>
-        Blablsalfkakfopkfowgieg
-    </h4>
+    <div class="container-fluid">
+        <p class="text-left">
+        <h3>Vitajte na stránke bazáru.</h3><br>
+        Bazár poskytuje priestor pre inzerciu. <br>
+        Používateľ po registrácii môže vytvárať inzeráty, následne ich upravovať alebo mazať.<br><br>
+        Prevádzkovateľ si vyhraduje právo mazať inzeráty, ktoré sú v rozpore s právnymi predpismi a dobrými mravmi.<br>
+        Inzeráty sú rozdelené podľa kategórií.<br><br>
+
+        V prípade chýb sa ozvite na email: mmmesiarovamaja@gmail.com.<br>
+        Ďakujeme za porozumenie a v prípade otázok nás neváhajte kontaktovať.<br><br><br>
+
+        Váš tím Bazáru</p><br>
+
+        </p>
+    </div>
 @endsection
--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.layout')
 
 @section('content')
+    <div class="container-fluid">
+        <h3> Kontakt </h3>
+        <p>Bazár prevádzkuje fyzická osoba Mária Mäsiarová.<br>
+            Pokiaľ máte nejaký problém s vkladaním alebo mazaním inzerátu, kontaktujte nás:<br><br>
 
+            Podpora: mmmesiarovamaja@gmail.com </p><br>
+    </div>
 @endsection
