63125bb9262676af051a1e100b7dba13be2d483f
masiarova <mmmesiarovamaja@gmail.com>
2022-11-28 14:20:51 +0100

    about text

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,20 +1,37 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="container">
+    <div class="content-wrapper">
         <h3>Vitajte na stránke bazáru.</h3><br>
-        <p class="text-left">
-            Bazár poskytuje priestor pre inzerciu. <br>
+        <p class="text">
+            Bazár prevádzkuje fyzická osoba Mária Mäsiarová. <br>
+            Bazár poskytuje priestor pre inzerciu súkromníkov a firiem, avšak nie je predajcom tovaru. <br>
+            Informáciami o tovare disponuje predajca nie vlastník stránky, kontaktuje preto predajcu.
+            Kontaktné údaje sú uvedené pri pri inzeráte.
             Používateľ po registrácii môže vytvárať inzeráty, následne ich upravovať alebo mazať.<br><br>
             Prevádzkovateľ si vyhraduje právo mazať inzeráty, ktoré sú v rozpore s právnymi predpismi a dobrými
             mravmi.<br>
             Inzeráty sú rozdelené podľa kategórií.<br><br>
 
 
-            V prípade chýb sa ozvite na email: mmmesiarovamaja@gmail.com.<br>
+            Pre nahlásenie inzerátu využite priamo odkaz z inzerátu "nahlásiť chybu".
+            Pri nahlasovaní vyberte dôvod a čo najpresnejšie nám popíšte problém.
+
+            V prípade,
+            že boli porušené Vaše autorské alebo licenčné práva v inzeráte je potrebné nás kontaktovať písomne.
+
+
+            V prípade chýb kontaktujte nás admina: kontakt@bazar.com.<br>
             Ďakujeme za porozumenie a v prípade otázok nás neváhajte kontaktovať.<br><br><br>
 
             <span class="nicefont">Váš tím Bazáru</span>
+
         </p>
+        <footer>
+            <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script>, Bazar.sk</p>
+        </footer>
+
     </div>
+
 @endsection
+
